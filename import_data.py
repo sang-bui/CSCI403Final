3920e0b43aac8d5ea60103a900ffcf8b28c2e6ae
Sang Bui <sangbui1281@gmail.com>
2025-04-23 18:58:19 -0600

    Left Merge (Target Only US Colleges)

--- import_data.py
+++ import_data.py
@@ -10,7 +10,7 @@ try:
     csv_ds['name'] = csv_ds['name'].str.strip()
     xlsx_ds['name'] = xlsx_ds['name'].str.strip()
 
-    merged_ds = pd.merge(csv_ds, xlsx_ds, on="name",how="inner")
+    merged_ds = pd.merge(xlsx_ds, csv_ds, on="name", how="left")
 
     #connect through 'engine' 
     engine = create_engine("postgresql://thai_tran:Grayravens.1265@ada.mines.edu:5432/csci403")
